afa0409f9a215f3d499a2a4103d503e7ded4430a
tatiana <t_mamonovaa@mail.ru>
2016-10-17 20:43:34 +0500

    Homework 3

---
+++ homework 3/homework.py
@@ -0,0 +1,11 @@
+a=int(input("введите первое число "))
+b=int(input("введите второе число "))
+c=int(input("введите третье число "))
+if a+b==c:
+    print(a, "+", b, "=", c)
+else:
+    print(a, "+", b, "!=", c)
+if a/b==c:
+    print(a, "/", b, "=", c)
+else:
+    print("a, "+", b, "!=", c")
